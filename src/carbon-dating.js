8e7b4247b1f171d2706dd6d7660f4198889071b8
skelegor8 <skelegor8@gmail.com>
2022-05-02 23:15:28 +0300

    fix: fix bug

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,7 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(sampleActivity) {
+function dateSample(/*sampleActivity*/) {
   throw new NotImplementedError('Not implemented');
   let carbonDate = 0;
   if (Number(sampleActivity) == true && typeof  sampleActivity == String) {
